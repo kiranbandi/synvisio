b77a6f3a2355d8eea902acc641cfaddb413b142c
kiranbandi <bvenkatkiran@gmail.com>
2018-07-16 15:01:36 -0600

    selective zooming added for genomeview when chromosome mode is on

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,6 +1,7 @@
 export { default as NavBar } from './NavBar';
 export { default as Container } from './Container';
 export { default as Loader } from './Loader';
+export { default as ResetIcon } from './ResetIcon';
 export { default as Information } from './Information';
 export { default as FilterPanel } from './FilterPanel';
 export { default as PieChart } from './PieChart';
