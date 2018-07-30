7a03952bfcc4616008e3c166acd3ea1765ccc73f
kiranbandi <bvenkatkiran@gmail.com>
2018-07-30 10:17:18 -0600

    changed hive angles and automatic chromosome mode start when only one set of chromosomes are selected

--- src/components/HiveView/hiveAngles.js
+++ src/components/HiveView/hiveAngles.js
@@ -9,7 +9,7 @@ export default function(size) {
             return [-pi / 2, pi / 2];
         case 3:
             // 0 , 120 and 240 
-            return [0, (2 * pi) / 3, (4 * pi) / 3];
+            return [pi / 3, ((2 * pi) / 3) + (pi / 3), ((4 * pi) / 3) + (pi / 3)];
         default:
             //For higher orders simply divide the angles equally so take 360 and divide by size 
             return _.range(size).map((val) => (2 * pi * val) / size);
