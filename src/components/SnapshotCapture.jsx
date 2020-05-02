85dfc96302c62a1afa2ccf948c542f75c0d856f9
kiranbandi <bvenkatkiran@gmail.com>
2020-05-02 03:04:01 -0600

    snapshot updated with actual image

--- src/components/SnapshotCapture.jsx
+++ src/components/SnapshotCapture.jsx
@@ -3,6 +3,8 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { setSnapshotList } from '../redux/actions/actions';
 import toastr from '../utils/toastr';
+import ParseSVG from '../utils/ParseSVG';
+import svgSaver from '../utils/saveSVGasPNG';
 import uniqid from 'uniqid';
 
 class SnapshotCapture extends Component {
@@ -14,11 +16,25 @@ class SnapshotCapture extends Component {
 
     storeSnapshot() {
         let { snapshotList, configuration, setSnapshotList } = this.props, uniqueCode = uniqid();
-        //  Treading Dangerous Territory here by polluting the global name space 
-        //  But this reduces the load placed on the redux and react global store
-        window.synVisio.snapshotStore[uniqueCode] = _.cloneDeep(configuration);
-        setSnapshotList([...snapshotList, uniqueCode]);
-        toastr["info"]("Current state of visualization stored as a local snapshot", "SNAPSHOT STORED");
+
+        // Get the SVG element
+        let svgElements = document.getElementsByClassName('exportable-svg');
+        // check if there is a visual snapshot is available to be stored
+        if (svgElements.length > 0) {
+            ParseSVG(svgElements[0]).then((svgEl) => {
+                svgSaver.svgAsPngUri(svgEl, { 'scale': '0.5' })
+                    .then(uri => {
+                        //  Treading Dangerous Territory here by polluting the global name space 
+                        //  But this reduces the load placed on the redux and react global store
+                        window.synVisio.snapshotStore[uniqueCode] = { 'configuration': _.cloneDeep(configuration), 'imgURI': uri };;
+                        setSnapshotList([...snapshotList, uniqueCode]);
+                        toastr["info"]("Current state of visualization stored as a local snapshot", "SNAPSHOT STORED");
+                    });
+            });
+        }
+        else {
+            toastr["warning"]("No visualization available to store as a snapshot, generate one first", "SNAPSHOT ERROR");
+        }
     }
 
     render() {
