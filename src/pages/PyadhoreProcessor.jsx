7f03aa5501adc14b6bc97ddfdc85f79fbe71a7a9
kiranbandi <bvenkatkiran@gmail.com>
2020-08-23 01:54:56 -0600

    added support to process pyadhore output into synvisio input files

--- src/pages/PyadhoreProcessor.jsx
+++ src/pages/PyadhoreProcessor.jsx
@@ -4,6 +4,19 @@ import getFile from '../utils/getFile';
 import toastr from '../utils/toastr';
 import FileSaver from 'file-saver';
 
+
+var header = "############### Parameters ###############" + "\n" +
+    "# MATCH_SCORE: 0" + "\n" +
+    "# MATCH_SIZE: 0" + "\n" +
+    "# GAP_PENALTY: 0" + "\n" +
+    "# OVERLAP_WINDOW: 0" + "\n" +
+    "# E_VALUE: 0" + "\n" +
+    "# MAX GAPS: 0" + "\n" +
+    "############### Statistics ###############" + "\n" +
+    "# Number of collinear genes: 0, Percentage: 0" + "\n" +
+    "# Number of all genes: 0" + "\n" +
+    "##########################################" + "\n";
+
 export default class PyadhoreProcessor extends Component {
 
     constructor(props) {
@@ -16,6 +29,8 @@ export default class PyadhoreProcessor extends Component {
     onUpload = () => {
 
         this.setState({ isProcessing: true });
+
+        var datastore = {};
 
         // load the coordinate file
         getFile('genelist-file')
@@ -30,40 +45,45 @@ export default class PyadhoreProcessor extends Component {
 
                 var gffFileList = [];
                 _.map(groupedByChromosome, (cGroup) => {
-                    gffFileList.push([cGroup[0], cGroup[1], cGroup[5], cGroup[6]].join('\t'));
+                    _.map(cGroup, (geneEntry) => {
+                        gffFileList.push([geneEntry[1], geneEntry[0], geneEntry[5], geneEntry[6]]);
+                    })
                 });
-                var gffFileContent = gffFileList.join('\n');
-
-
-
+                saveFile(_.map(gffFileList, (d) => d.join('\t')).join('\n'), 'example.gff');
+                return Promise.resolve(gffFileList);
             })
-            // // store the data and then load the collinear file
-            // .then((data) => {
-            //     datastore = Object.assign(datastore, { ...data });
-            //     return getFile('collinear-file');
-            // })
-            // // process the collinear file
-            // .then(((response) => { return processFile(response, 'collinear') }))
-            // // store the collinear data and load the track file if one is provided
-            // .then((data) => {
+            // Process the pyadhore file and create a collinearity file
+            .then((data) => {
+                datastore = Object.assign(datastore, { 'gffFileList': data });
+                return getFile('pyadhore-file');
+            })
+            // process the pyadhore file
+            .then((adhoreResponse) => {
+                var adhoreLines = adhoreResponse.split('\n').slice(1).map((d) => d.split(',')),
+                    // filter out multiplicons of order 2 or above
+                    modifiedLines = _.map(_.filter(adhoreLines, (e) => e.length > 2), (d) => {
+                        d[10] = d[2].slice(0, 2) + d[3];
+                        return d;
+                    }),
+                    groupedByBlock = _.groupBy(modifiedLines, (d) => d[1]);
 
-            //     let { information, alignmentList, uniqueIDList } = data, { chromosomeMap } = datastore;
+                const alignmentText = [];
 
-            //     if (!showAllScaffolds) {
-            //         [...chromosomeMap].map((entry) => {
-            //             if (uniqueIDList.indexOf(entry[0]) == -1) {
-            //                 chromosomeMap.delete(entry[0]);
-            //                 console.log('deleted entry', entry[0]);
-            //             }
-            //         });
-            //     }
+                // filter only multiplicons of level 2
+                _.map(_.filter(groupedByBlock, (d) => d.length == 2), (alignment, aindex) => {
+                    const alignmentTitle = '## ' + 'Alignment '
+                        + aindex + ": " + "score=100 e_value=0 N=2 "
+                        + alignment[0][10] + '&' + alignment[1][10] + " plus";
+                    const alignmentContent1 = [aindex, alignment[0][4], alignment[1][4], '0'].join('\t');
+                    const alignmentContent2 = [aindex, alignment[0][5], alignment[1][5], '0'].join('\t');
+                    alignmentText.push([alignmentTitle, alignmentContent1, alignmentContent2].join('\n'));
+                });
 
-            //     datastore = Object.assign({}, datastore, { information, alignmentList, chromosomeMap });
-            //     console.log(datastore);
-
-            //     return trackFiles[0] ? getFile('track-file-0') : Promise.resolve(false);
-            // })
+                saveFile(header + alignmentText.join('\n'), 'example.collinearity');
+            })
             .catch(() => {
+                debugger;
+
                 toastr["error"]("Failed to process the files , Please try again.", "ERROR");
             })
             .finally(() => { this.setState({ isProcessing: false }) });
@@ -90,3 +110,8 @@ export default class PyadhoreProcessor extends Component {
     }
 };
 
+
+function saveFile(fileData, filename) {
+    var blob = new Blob([fileData], { type: "text/plain;charset=utf-8" });
+    FileSaver.saveAs(blob, filename);
+}
